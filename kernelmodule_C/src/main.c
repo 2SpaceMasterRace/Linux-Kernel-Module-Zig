310ba473fd01f7c0fb481adf30162aecb9791d1c
2SpaceMasterRace <alphanovachrono@gmail.com>
2024-09-20 19:24:35 +0530

    "Hello World" of linux programming

--- kernelmodule_C/src/main.c
+++ kernelmodule_C/src/main.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include "lib.c"
+/*
+simple kernel module
 
-int main() {
-  int result = add(40, 2);
-  printf("40 + 2 = %d\n", result);
+<linux/module.h> : Needed by all Linux Kernel Modules
+<linux/printk.h> : Needed for all pr_info()
+
+non 0 return would mean the init module has failed ; the module can't be loaded
+
+*/
+
+#include <linux/module.h>
+#include <linux.printk.h>
+
+int init_module(void){
+  pr_info("Hello World!\n");
   return 0;
 }
+
+void cleanup_module(void){
+  pr_info("Goodbye, world\n");
+}
+
+MODULE_LICENSE("GPL");
